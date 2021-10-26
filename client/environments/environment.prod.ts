636c8257ea566c2c7551573f83e566d33a76cdcb
Giuliano Marinelli <giuliano.marinelli@hotmail.com>
2021-10-26 15:30:10 -0300

    hotfix: updated url of apis

--- client/environments/environment.prod.ts
+++ client/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  metamodelUrl: 'http://crowd-cpre.fi.uncoma.edu.ar/',
-  metamodelVerbalizationUrl: 'http://crowd-test.fi.uncoma.edu.ar/',
-  metamodelOwlUrl: 'http://crowd.fi.uncoma.edu.ar/crowd2/reasoning/translate/metamodel2owl.php',
-  reasoningUrl: 'http://crowd.fi.uncoma.edu.ar/crowd2/reasoning/querying/metamodelsat.php'
+  metamodelUrl: 'https://crowd-cpre.fi.uncoma.edu.ar/',
+  metamodelVerbalizationUrl: 'https://crowd-test.fi.uncoma.edu.ar/',
+  metamodelOwlUrl: 'https://crowd.fi.uncoma.edu.ar/crowd2/reasoning/translate/metamodel2owl.php',
+  reasoningUrl: 'https://crowd.fi.uncoma.edu.ar/crowd2/reasoning/querying/metamodelsat.php'
 };
