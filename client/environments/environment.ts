433a0b0de13a0f4e9ff74751a30d9a07d26098d0
Giuliano Marinelli <giuliano.marinelli@hotmail.com>
2022-05-10 15:28:40 -0300

    updated environment

--- client/environments/environment.ts
+++ client/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  metamodelUrl: "http://localhost:3333/",//'https://crowd-core.fi.uncoma.edu.ar/',
+  metamodelUrl: 'https://crowd-core.fi.uncoma.edu.ar/',
   metamodelVerbalizationUrl: 'https://crowd-test.fi.uncoma.edu.ar/',
   metamodelOwlUrl: 'https://crowd.fi.uncoma.edu.ar/crowd2/reasoning/translate/metamodel2owl.php',
   reasoningUrl: 'https://crowd.fi.uncoma.edu.ar/crowd2/reasoning/querying/metamodelsat.php'
